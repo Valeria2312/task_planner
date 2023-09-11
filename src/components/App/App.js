f8517df8105dc841172c0d47c357123fce0e89eb
Valeria <Koroleva.Valeria.97@yandex.ru>
2023-09-11 12:44:18 +0300

    стили, шрифты, хедр

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from "../Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+      <main className="Container"></main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import "./Header.css"
+function Header() {
+    return (
+            <header className="header">
+                <div className="header-title">Task Planner For You</div>
+            </header>
+    );
+}
+
+export default Header;
